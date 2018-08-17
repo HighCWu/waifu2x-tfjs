af0146f935c7a7354f45ca4565d7c51976573be8
HighCWu <HighCWu@163.com>
2018-08-17 12:11:12 +0800

    add disposing for tensor

--- src/Waifu2x.js
+++ src/Waifu2x.js
@@ -1,5 +1,7 @@
 import { tf, Image } from './extend';
 
+let model_paths_obj = {};
+
 class _Waifu2x {
 
     constructor() {
@@ -8,12 +10,34 @@ class _Waifu2x {
 
     async __init__(model_paths) {
         this.params = [];
+        const await_loading =  (obj, index) => {
+            return new Promise((resolve) => {
+                if(obj[index] !== 'loading') {
+                    resolve(obj[index]);
+                } else {
+                    const interval = setInterval(()=>{
+                        if(obj[index] !== 'loading') {
+                            resolve(obj[index]);
+                            clearInterval(interval);
+                        }
+                    }, 1000)
+                }
+            });
+        };
         if(typeof(model_paths) === 'string') {
-            this.params.push(await (await fetch(model_paths)).json());
+            if(typeof(model_paths_obj[model_paths]) === 'undefined') {
+                model_paths_obj[model_paths] = 'loading...';
+                model_paths_obj[model_paths] = await (await fetch(model_paths)).json();
+            }
+            this.params.push(await await_loading(model_paths_obj,model_paths));
             return;
         }
         for(let path of model_paths) {
-            this.params.push(await (await fetch(path)).json());
+            if(typeof(model_paths_obj[path]) === 'undefined') {
+                model_paths_obj[path] = 'loading...';
+                model_paths_obj[path] = await (await fetch(path)).json();
+            }
+            this.params.push(await await_loading(model_paths_obj,path));
         }
     }
 
@@ -48,40 +72,70 @@ class _Waifu2x {
     }
 
     async _loadImageY(path, is_noise) {
-        let im = (await Image.open(path)).convert('YCbCr');
+        let im = await Image.open(path);
+        im = tf.tidy(()=>{
+            let _im = im.convert('YCbCr');
+            tf.dispose(im);
+            return _im;
+        })
 
-        if(is_noise) {
-            im = im.asType('float32');
-        } else {
-            im = tf.image.resizeNearestNeighbor(im, [im.shape[0]*2,im.shape[1]*2]).asType('float32');
-        }
+        im = tf.tidy(()=>{
+            let _im;
+            if(is_noise) {
+                _im = im.asType('float32');
+            } else {
+                _im = tf.image.resizeNearestNeighbor(im, [im.shape[0]*2,im.shape[1]*2]).asType('float32');
+            }
+            tf.dispose(im);
+            return _im;
+        });
         
-        let x = im.slice([0, 0, 0], [im.shape[0], im.shape[1], 1]).reshape([1, 1, im.shape[0], im.shape[1]]).div(tf.scalar(255.0))
-
-        return [im, x]
+        
+        let x = tf.tidy(()=>{
+            return im.slice([0, 0, 0], [im.shape[0], im.shape[1], 1]).reshape([1, 1, im.shape[0], im.shape[1]]).div(tf.scalar(255.0));
+        })
+        
+        return [im, x];
 
     }
 
     async _loadImageRGB(path, is_noise) {
         let im = await Image.open(path);
 
-        if(is_noise) {
-            im = im.asType('float32');
-        } else {
-            im = tf.image.resizeNearestNeighbor(im, [im.shape[0]*2,im.shape[1]*2]).asType('float32');
-        }
-        
-        let r = im.slice([0, 0, 0], [im.shape[0], im.shape[1], 1]);
-        let g = im.slice([0, 0, 1], [im.shape[0], im.shape[1], 1]);
-        let b = im.slice([0, 0, 2], [im.shape[0], im.shape[1], 1]);
-        let x = tf.stack([r,g,b]).reshape([1, 3, im.shape[0], im.shape[1]]).div(tf.scalar(255.0))
+        im = tf.tidy(()=>{
+            let _im;
+            if(is_noise) {
+                _im = im.asType('float32');
+            } else {
+                _im = tf.image.resizeNearestNeighbor(im, [im.shape[0]*2,im.shape[1]*2]).asType('float32');
+            }
+            tf.dispose(im);
+            return _im;
+        });
+
+        let x = tf.tidy(()=>{
+            let r = im.slice([0, 0, 0], [im.shape[0], im.shape[1], 1]);
+            let g = im.slice([0, 0, 1], [im.shape[0], im.shape[1], 1]);
+            let b = im.slice([0, 0, 2], [im.shape[0], im.shape[1], 1]);
+            return tf.stack([r,g,b]).reshape([1, 3, im.shape[0], im.shape[1]]).div(tf.scalar(255.0));
+        });
         
         return [im, x]
 
     }
 
     async generate(img_path, param_id=0, is_noise=false) {
-        const input_channel = this.params[param_id][0]['nInputPlane']
+        const input_channel = this.params[param_id][0]['nInputPlane'];
+        
+        if(!this.models) {
+            this.models = []
+            for(let i in this.params) {
+                const t_input_channel = this.params[i][0]['nInputPlane'];
+                this.models.push(this._loadModel([t_input_channel, null, null], i));
+            }
+        }
+        // Define model from the image.
+        let model = this.models[param_id];
 
         let im, x;
         // Loading image from img_path.
@@ -90,27 +144,30 @@ class _Waifu2x {
         } else {
             [im, x] = await this._loadImageRGB(img_path, is_noise);
         }
-        // Define model from the image.
-        let model = this._loadModel([input_channel, null, null], param_id);
         
-        // Generate new value.
-        let y = model.predict(x);
-
-        // Return value to image.
-        if(input_channel === 1) {
-            y = y.mul(tf.scalar(255.0)).clipByValue(0, 255).reshape([im.shape[0],im.shape[1], 1]);
-            let cb = im.slice([0, 0, 1], [im.shape[0],im.shape[1], 1]);
-            let cr = im.slice([0, 0, 2], [im.shape[0],im.shape[1], 1]);
-            im = tf.stack([y,cb,cr],-1).reshape([im.shape[0],im.shape[1],3]);
-            im = im.setMode('YCbCr').convert('RGB').asType('int32');
-        } else {
-            im = y.mul(tf.scalar(255.0)).clipByValue(0, 255);
-            let r = im.slice([0, 0, 0, 0], [1, 1, im.shape[2], im.shape[3]]);
-            let g = im.slice([0, 1, 0, 0], [1, 1, im.shape[2], im.shape[3]]);
-            let b = im.slice([0, 2, 0, 0], [1, 1, im.shape[2], im.shape[3]]);
-            im = tf.stack([r,g,b],-1).reshape([im.shape[2],im.shape[3], 3]);
-            im = im.setMode('RGB').asType('int32');
-        }
+        im = tf.tidy(()=>{
+            let _im = im;// Generate new value.
+            let y = model.predict(x);
+    
+            // Return value to image.
+            if(input_channel === 1) {
+                y = y.mul(tf.scalar(255.0)).clipByValue(0, 255).reshape([im.shape[0],im.shape[1], 1]);
+                let cb = im.slice([0, 0, 1], [im.shape[0],im.shape[1], 1]);
+                let cr = im.slice([0, 0, 2], [im.shape[0],im.shape[1], 1]);
+                im = tf.stack([y,cb,cr],-1).reshape([im.shape[0],im.shape[1],3]);
+                im = im.setMode('YCbCr').convert('RGB').asType('int32');
+            } else {
+                im = y.mul(tf.scalar(255.0)).clipByValue(0, 255);
+                let r = im.slice([0, 0, 0, 0], [1, 1, im.shape[2], im.shape[3]]);
+                let g = im.slice([0, 1, 0, 0], [1, 1, im.shape[2], im.shape[3]]);
+                let b = im.slice([0, 2, 0, 0], [1, 1, im.shape[2], im.shape[3]]);
+                im = tf.stack([r,g,b],-1).reshape([im.shape[2],im.shape[3], 3]);
+                im = im.setMode('RGB').asType('int32');
+            }
+            tf.dispose(_im);
+            return im;
+        })
+        
 
         let canvas = document.createElement('canvas');
         canvas.height = im.shape[0];
@@ -123,6 +180,14 @@ class _Waifu2x {
         canvas = null;
 
         return im;
+    }
+
+    tidy() {
+        for(let model of this.models) {
+            for (let layer of model.layers) {
+                tf.dispose(layer.getWeights());
+            }
+        }
     }
 
 }
--- src/extend.js
+++ src/extend.js
@@ -37,83 +37,87 @@ tf.Tensor.prototype.setMode = function(mode) {
     return this;
 }
 tf.Tensor.prototype.convertToRGB = function() {
-    const tensor = this.asType('float32');
-    let rlt;
-    switch (this.mode) {
-        case 'RGB':
-            rlt = this.clone();
-            break;
-
-        case 'YCbCr':
-            let y = tensor.slice([0, 0, 0], [tensor.shape[0], tensor.shape[1], 1]);
-            let cb = tensor.slice([0, 0, 1], [tensor.shape[0], tensor.shape[1], 1]);
-            let cr = tensor.slice([0, 0, 2], [tensor.shape[0], tensor.shape[1], 1]);
-            let r = y.add(tf.scalar(-16)).mul(tf.scalar(1.164))
-                        .add(
-                            cr.add(tf.scalar(-128)).mul(tf.scalar(1.596))
-                        )
-            let g = y.add(tf.scalar(-16)).mul(tf.scalar(1.164))
-                        .add(
-                            cb.add(tf.scalar(-128)).mul(tf.scalar(-0.392))
-                        )
-                        .add(
-                            cr.add(tf.scalar(-128)).mul(tf.scalar(-0.813))
-                        )
-            let b = y.add(tf.scalar(-16)).mul(tf.scalar(1.164))
-                        .add(
-                            cb.add(tf.scalar(-128)).mul(tf.scalar(2.017))
-                        )
-            rlt = tf.stack([r,g,b],-1).reshape([tensor.shape[0],tensor.shape[1],3]).clipByValue(0, 255);
-            break;
-    
-        default:
-            break;
-    }
-    
-    if(rlt) {
-        rlt.mode = 'RGB';
-    }
-    return rlt; // default 'float32'
-};
-tf.Tensor.prototype.convert = function(mode) {
-    if(this.mode === mode) {
-        return this.clone();
-    } else {
-        const tensor = this.convertToRGB().asType('float32');
+    return tf.tidy(()=>{
+        const tensor = this.asType('float32');
         let rlt;
-        switch (mode) {
+        switch (this.mode) {
             case 'RGB':
-                rlt = tensor;
+                rlt = this.clone();
                 break;
-
+    
             case 'YCbCr':
-                let r = tensor.slice([0, 0, 0], [tensor.shape[0], tensor.shape[1], 1]);
-                let g = tensor.slice([0, 0, 1], [tensor.shape[0], tensor.shape[1], 1]);
-                let b = tensor.slice([0, 0, 2], [tensor.shape[0], tensor.shape[1], 1]);
-                let y = r.mul(tf.scalar(0.257))
-                                .add(g.mul(tf.scalar(0.564)))
-                                .add(b.mul(tf.scalar(0.098))
-                                .add(tf.scalar(16)));
-                let cb = r.mul(tf.scalar(-0.148))
-                                .add(g.mul(tf.scalar(-0.291)))
-                                .add(b.mul(tf.scalar(0.439))
-                                .add(tf.scalar(128)));
-                let cr = r.mul(tf.scalar(0.439))
-                                .add(g.mul(tf.scalar(-0.368)))
-                                .add(b.mul(tf.scalar(-0.071))
-                                .add(tf.scalar(128)));
-                rlt = tf.stack([y,cb,cr],-1).reshape([tensor.shape[0],tensor.shape[1],3]);
+                let y = tensor.slice([0, 0, 0], [tensor.shape[0], tensor.shape[1], 1]);
+                let cb = tensor.slice([0, 0, 1], [tensor.shape[0], tensor.shape[1], 1]);
+                let cr = tensor.slice([0, 0, 2], [tensor.shape[0], tensor.shape[1], 1]);
+                let r = y.add(tf.scalar(-16)).mul(tf.scalar(1.164))
+                            .add(
+                                cr.add(tf.scalar(-128)).mul(tf.scalar(1.596))
+                            )
+                let g = y.add(tf.scalar(-16)).mul(tf.scalar(1.164))
+                            .add(
+                                cb.add(tf.scalar(-128)).mul(tf.scalar(-0.392))
+                            )
+                            .add(
+                                cr.add(tf.scalar(-128)).mul(tf.scalar(-0.813))
+                            )
+                let b = y.add(tf.scalar(-16)).mul(tf.scalar(1.164))
+                            .add(
+                                cb.add(tf.scalar(-128)).mul(tf.scalar(2.017))
+                            )
+                rlt = tf.stack([r,g,b],-1).reshape([tensor.shape[0],tensor.shape[1],3]).clipByValue(0, 255);
                 break;
         
             default:
                 break;
         }
-
+        
         if(rlt) {
-            rlt.mode = mode;
+            rlt.mode = 'RGB';
         }
         return rlt; // default 'float32'
-    }
+    })
+};
+tf.Tensor.prototype.convert = function(mode) {
+    return tf.tidy(()=>{
+        if(this.mode === mode) {
+            return this.clone();
+        } else {
+            const tensor = this.convertToRGB().asType('float32');
+            let rlt;
+            switch (mode) {
+                case 'RGB':
+                    rlt = tensor;
+                    break;
+    
+                case 'YCbCr':
+                    let r = tensor.slice([0, 0, 0], [tensor.shape[0], tensor.shape[1], 1]);
+                    let g = tensor.slice([0, 0, 1], [tensor.shape[0], tensor.shape[1], 1]);
+                    let b = tensor.slice([0, 0, 2], [tensor.shape[0], tensor.shape[1], 1]);
+                    let y = r.mul(tf.scalar(0.257))
+                                    .add(g.mul(tf.scalar(0.564)))
+                                    .add(b.mul(tf.scalar(0.098))
+                                    .add(tf.scalar(16)));
+                    let cb = r.mul(tf.scalar(-0.148))
+                                    .add(g.mul(tf.scalar(-0.291)))
+                                    .add(b.mul(tf.scalar(0.439))
+                                    .add(tf.scalar(128)));
+                    let cr = r.mul(tf.scalar(0.439))
+                                    .add(g.mul(tf.scalar(-0.368)))
+                                    .add(b.mul(tf.scalar(-0.071))
+                                    .add(tf.scalar(128)));
+                    rlt = tf.stack([y,cb,cr],-1).reshape([tensor.shape[0],tensor.shape[1],3]);
+                    break;
+            
+                default:
+                    break;
+            }
+    
+            if(rlt) {
+                rlt.mode = mode;
+            }
+            return rlt; // default 'float32'
+        }
+    })
 };
 
 export {
